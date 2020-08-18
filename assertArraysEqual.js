78ad313d3538eb31853e3e8870c2202b021d653a
David Ramalho <david.m.ramalho@gmail.com>
2020-08-18 23:44:23 +0000

    playing with arrays and assertions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -5,6 +5,7 @@ const assertEqual = function(actual, expected) {
     return console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -13,6 +14,14 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return console.log(`✌ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    return console.log(`⛔ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
