4b05965cd1f589540d5abb7d6895ee3b2b39fe76
David Ramalho <david.m.ramalho@gmail.com>
2020-08-19 21:26:54 +0000

    looping through objects driving me nuts :)

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✌ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string){
+  result = {};
+  for (item of string){
+    if (result[item]){
+      result[item] += 1;
+    } else {
+      result[item] = 1;
+    }
+  }
+  return result;
+}
+
+console.log(countLetters('david'));
+let test = countLetters('this is a test string')
+assertEqual(test['t'], 4);
