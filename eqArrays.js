04785264478e55bdf53f341cfe5929774afc1a14
David Ramalho <david.m.ramalho@gmail.com>
2020-08-24 21:18:06 +0000

    lotide tests refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✌ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i] || array1.length !== array2.length) {
@@ -14,6 +9,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+module.exports = eqArrays;
