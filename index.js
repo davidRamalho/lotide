003d740d09fee1c859e39cab05a9a5eefe1126e1
David Ramalho <david.m.ramalho@gmail.com>
2020-08-24 23:58:50 +0000

    used short hand ES6 to name export object keys

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const countLetters = require('./countLetters');
@@ -14,18 +14,18 @@ const takeUntil = require('./takeUntil')
 const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  countLetters: countLetters,
-  countOnly: countOnly, 
-  eqObjects: eqObjects, 
-  findKey: findKey, 
-  findKeyByValue: findKeyByValue, 
-  flatten: flatten, 
-  letterPositions: letterPositions, 
-  map: map, 
-  takeUntil: takeUntil, 
-  without: without
+  head,
+  tail,
+  middle,
+  eqArrays,
+  countLetters,
+  countOnly, 
+  eqObjects, 
+  findKey, 
+  findKeyByValue, 
+  flatten, 
+  letterPositions, 
+  map, 
+  takeUntil, 
+  without
 };
