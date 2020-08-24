aec172f0119f75e60ed9f46fa5a0b87200f7320a
David Ramalho <david.m.ramalho@gmail.com>
2020-08-24 22:19:32 +0000

    refactoring tests

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] || array1.length !== array2.length) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    return console.log(`✌ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   middleElements = [];
   if (array.length % 2 === 0){
@@ -26,5 +9,4 @@ const middle = function(array) {
   return middleElements;
 }
 
-console.log(middle([1, 2, 3, 4, 5, 6]))
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
